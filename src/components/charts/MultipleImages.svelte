2c3edeba678e4aa2cb60be6164a1f352a715e1e8
arudolph <alex3r@me.com>
2022-02-23 22:29:13 -0800

    first commit

---
+++ src/components/charts/MultipleImages.svelte
@@ -0,0 +1,41 @@
+<script>
+    import ChartDesc from "./ChartDesc.svelte";
+
+    export let link;
+    export let title;
+    export let src;
+    export let desc;
+    export let date;
+</script>
+
+<a href={link} target="_blank">
+    <div>
+        <div>
+            <ChartDesc title={title} desc={desc} date={date} />
+        </div>
+        {#each src as s, i}    
+            <img src={s} alt={"image for " + title} class={i === 1 ? "two" : ""}/>
+        {/each}
+    </div>
+</a>
+
+<style>
+    div > * {
+        width: 50%;
+    }
+
+    img.two {
+        height: 100%;
+    }
+
+    a:hover, a:active, a:link, a:visited {
+        text-decoration: none;
+    }
+
+    div {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Header from '../components/layout/Header.svelte';
+	import Footer from '../components/layout/Footer.svelte';
+</script>
+
+<svelte:body />
+
+<main>
+	<Header />
+	<div>
+		<article>
+			<slot />
+		</article>
+	</div>
+	<Footer />
+</main>
+
+<style>
+	div {
+		padding: 10px;
+		display: flex;
+		justify-content: center;
+	}
+	article {
+		width: 700px;
+	}
+	@media (max-width: 700px) {
+		article {
+			width: 100%;
+		}
+	}
+	:global(body) {
+		margin: 0px;
+		color: #333;
+		font-family: -apple-system, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+	:global(a) {
+		color: rgb(0, 100, 200);
+		text-decoration: none;
+	}
+</style>
